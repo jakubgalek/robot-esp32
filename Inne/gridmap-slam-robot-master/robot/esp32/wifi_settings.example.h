21e9eb5f14cbcafd4bd99a0eee8fc89918bdd5f0
Jakub Gałek <49043385+jakubgalek@users.noreply.github.com>
2025-04-12 23:43:20 +0200

    Added new schemes

--- Inne/gridmap-slam-robot-master/robot/esp32/wifi_settings.example.h
+++
@@ -1,9 +0,0 @@
-// This file contains the SSID and password information for your Wifi network
-
-const char* ssid = "SSID";
-const char* password = "password";
-
-
-// SSID and password for SoftAP Network
-const char* apSSID = "Robot";
-const char* apPwd = "robotwifi";
